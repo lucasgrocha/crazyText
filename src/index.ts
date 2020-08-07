ccbdb287a42ac67d4b4d5fd0087d226f94b01321
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-08-07 20:57:08 -0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+const convertText = (inputValue: string) => {
+  if (inputValue.length === 0) return;
+
+  let wordOfPhrase = inputValue.split(' ');
+  const newArr = [];
+
+  for (const word of wordOfPhrase) {
+    newArr.push(converted(word));
+  }
+  return newArr.join;
+};
+
+const converted = (word: string) => {
+  let c = 0;
+  let newWord = '';
+  for (const letter of word) {
+    if (c % 2 === 0) {
+      newWord += letter.toUpperCase();
+    } else {
+      newWord += letter.toLowerCase();
+    }
+
+    c++;
+  }
+  return newWord;
+};
+
+export default convertText;
